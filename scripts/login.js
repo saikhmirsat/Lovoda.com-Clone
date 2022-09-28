6fb0737ef56e26415904c61d0bd504b5363fd610
Kishan0431 <Kishanvjoshi1998@gmail.com>
2022-09-28 23:26:53 +0530

    Add Signup and login page

---
+++ scripts/login.js
@@ -0,0 +1,25 @@
+let login = ()=>{
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    let data = JSON.parse(localStorage.getItem("users")) || [];
+
+    for(let i=0; i<data.length; i++){
+        if(data[i].email === email){
+            if(data[i].password === password){
+                localStorage.setItem("users",JSON.stringify(true));
+                alert("Login successful!");
+                return;
+            }
+            else{
+                console.log(data[i].password);
+                console.log(password);
+                alert("Wrong credencial");
+                return;
+            }
+        }
+    }
+
+    alert("User doesn't exist, Sign up");
+
+}
