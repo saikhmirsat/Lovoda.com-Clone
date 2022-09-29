55402f84413e9e8f1d1e3092b1f8796ddd3bee71
saikhmirsat <saikh.mirsat.786@gmail.com>
2022-09-30 01:15:56 +0530

    update my code and apend items

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,184 @@
 
-let getData = async () => {
-    let url = "https://radiant-inlet-76290.herokuapp.com/homeproducts"
+let productData = [
+    {
+        image: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101104-1_1.jpg?v=1659450395",
+        image2: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101104-3_1_1426x.jpg?v=1659450395",
+        image3: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101104-2_1.jpg?v=1659450395",
+        title: "Evermore Ear Cuff Earrings",
+        dolar: 10.00,
+        id: 1
+    },
+    {
+        image: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101103-2_1.jpg?v=1659450313",
+        image2: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101103-3_1_1426x.jpg?v=1659450313",
+        image3: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101103-1_1.jpg?v=1659450313",
+        title: "Wired Ear Cuff Earrings",
+        dolar: 10.00,
+        id: 2
+    },
+    {
+        image: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101102-1_1.jpg?v=1659450226",
+        image2: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101102-4_1_1426x.jpg?v=1659450231",
+        image3: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101102-2_1.jpg?v=1659450231",
+        title: "Fern Ear Cuff Earrings",
+        dolar: 10.00,
+        id: 3
+    },
+    {
+        image: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101101-1_1.jpg?v=1659450051",
+        image2: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101101-3_1_1426x.jpg?v=1659450054",
+        image3: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101101-2_1.jpg?v=1659450054",
+        title: "Blossom Ear Cuff Earrings",
+        dolar: 10.00,
+        id: 4
+    },
+    {
+        image: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101108-3_1.jpg?v=1659449973",
+        image2: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101108-4_1_1426x.jpg?v=1659449973",
+        image3: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101108-1_1.jpg?v=1659449973",
+        title: "Barrel Stone Earrings",
+        dolar: 18.00,
+        id: 5
+    },
+    {
+        image: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101107-G-2_1.jpg?v=1659449908",
+        image2: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101107-G-3_1_1426x.jpg?v=1659449908",
+        image3: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101107-G-1_1.jpg?v=1659449908",
+        title: "Ball Stud Earrings | Gold",
+        dolar: 12.00,
+        id: 6
+    },
+    {
+        image: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101106-G-1_1.jpg?v=1659449836",
+        image2: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101106-G-4_1.jpg?v=1659449841",
+        image3: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101106-G-2_1.jpg?v=1659449841",
+        title: "Classic Stone Stud Earrings | Gold",
+        dolar: 12.00,
+        id: 7
+    },
+    {
+        image: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101105-1_1_1426x.jpg?v=1659449763",
+        image2: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101105-4_1.jpg?v=1659449765",
+        image3: "https://cdn.shopify.com/s/files/1/0627/7388/7215/products/L101105-2_1_1426x.jpg?v=1659449765",
+        title: "Acclaim Hoop Earrings",
+        dolar: 20.00,
+        id: 8
+    }
+];
 
-    let res = await fetch(url)
-    let data = await res.json()
-    renderDom(data)
-    console.log(data)
-}
-getData()
+let gridData = [
+    {
+        image: "https://scontent.cdninstagram.com/v/t51.2885-15/296817431_163686646176589_9025849763226676913_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=101&_nc_ohc=SlWJR_Uc8iUAX8AK_on&edm=AMO9-JQAAAAA&ccb=7-5&oh=00_AT9p3D4WbvFfbnnVL4fsOssfJGj1K_MbND0bXT2Z7yeeXA&oe=63365387&_nc_sid=b9f2ee"
+    },
+    {
+        image: "https://scontent.cdninstagram.com/v/t51.2885-15/292524868_710978926641964_7932405403123906811_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=vL0mLt-kDpwAX-JTLuz&edm=AMO9-JQAAAAA&ccb=7-5&oh=00_AT8oRDUke5id3ogMJyP9WYi0NPrhIBHFMajjDokbGkwCBQ&oe=63365799&_nc_sid=b9f2ee"
+    }, {
+        image: "https://scontent.cdninstagram.com/v/t51.2885-15/286870102_1070652883832328_5078147322534170043_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=100&_nc_ohc=AY365dKKgeIAX8ZmSki&edm=AMO9-JQAAAAA&ccb=7-5&oh=00_AT91HQUA8Hb9Gq1bfT-0zld2Dwb7M3xn434bS0fwJhP3Hw&oe=6336525C&_nc_sid=b9f2ee"
+    }, {
+        image: "https://scontent.cdninstagram.com/v/t51.2885-15/300949023_798468481336632_4767080338575120330_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=100&_nc_ohc=9YDbspN6npIAX_bpjcU&edm=AMO9-JQAAAAA&ccb=7-5&oh=00_AT-HgjNYNfwGcwwDhGFp8ZqJwSJK0h5jhV59d0umHVMkHA&oe=6336134B&_nc_sid=b9f2ee"
+    }, {
+        image: "https://scontent.cdninstagram.com/v/t51.2885-15/286632669_1734618116888586_6026357310411072155_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=106&_nc_ohc=k4T4HnqgttkAX9st3dU&edm=AMO9-JQAAAAA&ccb=7-5&oh=00_AT9zagg3TiQjmnGM9LR5QeQHbjLtfffv_u7lC3jU6EaCpA&oe=63363705&_nc_sid=b9f2ee"
+    }, {
+        image: "https://scontent.cdninstagram.com/v/t51.2885-15/286298243_398974155499479_8698678661107104004_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=wWTEcA-JwG4AX9QPXDo&edm=AMO9-JQAAAAA&ccb=7-5&oh=00_AT9-Xp4BX3Agb-c_yyUtM-2LbvpBPBsqE3RwjQt987j-Rg&oe=63363522&_nc_sid=b9f2ee"
+    }, {
+        image: "https://scontent.cdninstagram.com/v/t51.2885-15/285504612_2894217000871262_2042944149845897065_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=102&_nc_ohc=EHvy8T68rOEAX-o0_Su&edm=AMO9-JQAAAAA&ccb=7-5&oh=00_AT8SFdRDBAkw8cspoI192--Lo0RVlH1jf9w0cC5lvfI2Mw&oe=63365345&_nc_sid=b9f2ee"
+    }, {
+        image: "https://scontent.cdninstagram.com/v/t51.2885-15/285218666_2081961325319693_6730231299075407859_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=105&_nc_ohc=PJwvPAZZqjIAX9kfPb8&edm=AMO9-JQAAAAA&ccb=7-5&oh=00_AT9vN4rhi1iq_lqmP_UH76xYMrUciapxcZAQiyYUau3hhQ&oe=63365284&_nc_sid=b9f2ee"
+    }, {
+        image: "https://scontent.cdninstagram.com/v/t51.2885-15/285401021_552984419647108_3992250782296132999_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=Nd9vjBnWIwUAX_sRgmQ&edm=AMO9-JQAAAAA&ccb=7-5&oh=00_AT_lrbKIf82Qiu9ruovx1dy4PX_R7nS3-uAmgf5LfpfynQ&oe=63364CBD&_nc_sid=b9f2ee"
+    }, {
+        image: "https://scontent.cdninstagram.com/v/t51.2885-15/285100621_370858071774043_389166769621200107_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=107&_nc_ohc=0XHlA88gXcYAX-y9mvY&edm=AMO9-JQAAAAA&ccb=7-5&oh=00_AT-qN2oQSmKoxJuaBFCz5ia7GGevLQWMURa-4wG_TSwf7A&oe=63364FF1&_nc_sid=b9f2ee"
+    }, {
+        image: "https://scontent.cdninstagram.com/v/t51.2885-15/284728279_740278007407448_5057299864085407895_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=108&_nc_ohc=1F84nFiZLJAAX_1eZI9&edm=AMO9-JQAAAAA&ccb=7-5&oh=00_AT_LX64g5Sc9p8rowOKnohilkIOXuHiPIhJmRaUR8BjttA&oe=63365346&_nc_sid=b9f2ee"
+    }, {
+        image: "https://scontent.cdninstagram.com/v/t51.2885-15/284539193_707715653774700_2611001269885991634_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=100&_nc_ohc=oJNCsUwWOWAAX-Nl6Iv&edm=AMO9-JQAAAAA&ccb=7-5&oh=00_AT_nCum08f2khn24Jnmkt2lc2-DsxHGbgn78X0Z8sslf2w&oe=6336070E&_nc_sid=b9f2ee"
+    }, {
+        image: "https://scontent.cdninstagram.com/v/t51.2885-15/284289186_339103058332970_3046572654450733955_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=102&_nc_ohc=6aRGYi75AOkAX-OX59t&edm=AMO9-JQAAAAA&ccb=7-5&oh=00_AT-_E_WeykrunWdk71atp91-gCPCBEtALKvnWjbEn2pEOQ&oe=633637DB&_nc_sid=b9f2ee"
+    }, {
+        image: "https://scontent.cdninstagram.com/v/t51.2885-15/283922296_388804979929625_6823421441872576207_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=105&_nc_ohc=WYYgyUFKls0AX9KS73a&edm=AMO9-JQAAAAA&ccb=7-5&oh=00_AT8l8RkB5X_PlXt-5RrPPtNe-yzVBnzoYTU8ZvosDU_JHQ&oe=633637A3&_nc_sid=b9f2ee"
+    }
+]
+
 
 
 let renderDom = (data) => {
-    let container = document.getElementById("container-1")
+    let container = document.getElementById("product-list")
+    container.innerHTML = null
 
-    data.forEach(({ image, title, dolar }) => {
-        let div = document.createElement('div')
+    data.forEach((el) => {
+        let card = document.createElement("div");
+        card.setAttribute("class", "card");
 
-        const wishlist = document.createElement('img');
-        wishlist.src = "https://i.ibb.co/hFhZgVQ/Screenshot-2022-09-28-at-9-31-43-PM.png"
-        wishlist.setAttribute('id', 'wish')
+        card.addEventListener("click", () => {
+            localStorage.setItem("productDetails", JSON.stringify(el));
+            window.location.href = "Product.html";
+        });
 
-        const Img = document.createElement('img');
-        Img.src = image;
+        let love_btn = document.createElement("div");
+        love_btn.setAttribute("class", "heart-back");
+        let loveicon = document.createElement("img")
+        loveicon.src = "https://i.ibb.co/hFhZgVQ/Screenshot-2022-09-28-at-9-31-43-PM.png"
+        love_btn.append(loveicon)
 
-        const Name = document.createElement('p');
-        Name.innerText = title;
-        Name.setAttribute('class', 'title')
+        let img_box = document.createElement("div");
+        img_box.setAttribute("class", "img-box");
 
-        const Dolar = document.createElement('p');
-        Dolar.innerText = "$" + dolar + ".00"
+        let img = document.createElement("img");
+        img.src = el.image;
+        // img.loading = "lazy";
+        img_box.append(img);
 
-        div.append(wishlist, Img, Name, Dolar)
-        container.append(div)
+        let Title = document.createElement("a");
+        Title.innerText = el.title;
+        Title.setAttribute("class", "prod-title");
 
+        card.addEventListener("mouseover", (e) => {
+            img.src = el.image2;
+            Title.style.borderBottom = "1px solid #121212";
+        });
+        card.addEventListener("mouseout", (e) => {
+            img.src = el.image;
+            Title.style.borderBottom = "1px solid transparent";
+        });
+
+        let price = document.createElement("p");
+        price.innerText = `$${el.dolar.toFixed(2)}`;
+        price.setAttribute("class", "prod-price");
+
+        card.append(love_btn, img_box, Title, price);
+        container.append(card)
     });
-
-
 }
+renderDom(productData)
+
+let moreDataFuntion = (data) => {
+    let grid_img_list = document.getElementById("grid-img-list");
+
+    let shop_feed_list = document.createElement("div");
+    shop_feed_list.setAttribute("id", "shop-feed-list");
+
+    data.forEach((item) => {
+        let div = document.createElement("div");
+        div.addEventListener("click", () => {
+            window.location.href = "shop-all.html";
+        });
+
+        let img = document.createElement("img");
+        img.src = item.image;
+        img.setAttribute("class", "grid-img");
+
+        div.append(img);
+        shop_feed_list.append(div);
+    });
+    grid_img_list.append(shop_feed_list);
+};
+
+moreDataFuntion(gridData);
+
+
+
+let more_btn = document.getElementById("more");
+more_btn.addEventListener("click", (e) => {
+    moreDataFuntion(gridData);
+});
 
