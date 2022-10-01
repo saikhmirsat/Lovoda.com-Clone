6f0578c2cfa689953caa25e8ae01273fafede070
SNaNdii <singhnandita73@gmail.com>
2022-10-02 01:56:53 +0530

    update cart JS

--- scripts/Cart.js
+++ scripts/Cart.js
@@ -1,17 +1,17 @@
-
+let id;
 let cartItem = JSON.parse(localStorage.getItem("cartItem"))
 //----------Cart  Item  Show--------------------------------------
 function showdata(){
   let products = document.getElementById("products-list");
   products.innerHTML = "";
 
-  cartItem.forEach((el) => {
+  cartItem.forEach((el, index) => {
     let card = document.createElement("div");
     card.setAttribute("class", "card");
-    card.addEventListener("click", () => {
-      localStorage.setItem("productDetails", JSON.stringify(el));
-      window.location.href = "product.html";
-    });
+    // card.addEventListener("click", () => {
+    //   localStorage.setItem("productDetails", JSON.stringify(el));
+    //   window.location.href = "product.html";
+    // });
 
     let Small_card = document.createElement("div");
 
@@ -19,7 +19,7 @@ function showdata(){
     love_btn.setAttribute("class", "heart-back");
     love_btn.innerHTML = `<i class="fa fa-heart-o" aria-hidden="true"></i>`;
 
-    love_btn.addEventListener("click", (e) => {
+    love_btn.addEventListener("click", (el) => {
       love_btn.innerHTML = `<i class="fa fa-heart" aria-hidden="true"></i>`;
     });
 
@@ -38,10 +38,10 @@ function showdata(){
     title.setAttribute("class", "prod-title");
 
     //------------hover effect------------------------------
-    card.addEventListener("mouseout", (e) => {
-      img.src = el.image;
-      title.style.borderBottom = "1px solid transparent";
-    });
+    // card.addEventListener("mouseout", (el) => {
+    //   img.src = el.image;
+    //   title.style.borderBottom = "1px solid transparent";
+    // });
 
     let rem_div = document.createElement("div")
     let price = document.createElement("p");
@@ -51,6 +51,9 @@ function showdata(){
     let rem = document.createElement("button");
     rem.innerText = "Remove"
     rem.id = "remove"
+    rem.onclick = () => {
+      remove(index);
+    }
     rem_div.append(price, rem)
 
     card.append(Small_card, title, rem_div);
@@ -62,6 +65,18 @@ showdata()
 //---------------Total Item-----------------------------
 function TotalItem(){
   let item = document.getElementById("total");
-  item.innerText = cartItem.length
+  item.innerText = cartItem.length;
 }
 TotalItem()
+
+//--------------Sub-Total--------------------
+function SubTotal(){
+  
+}
+
+//-----------Remove---------------------------
+let remove = (id) => {
+  cartItem.splice(id, 1);
+  localStorage.setItem("cartItem", JSON.stringify(cartItem));
+  showdata()
+}
