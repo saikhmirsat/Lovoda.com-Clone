a2f15383eb89ee8c6e29ead7cc334928c4df8b5c
SNaNdii <singhnandita73@gmail.com>
2022-10-02 13:22:35 +0530

    update

--- scripts/Cart.js
+++ scripts/Cart.js
@@ -1,5 +1,7 @@
 let id;
 let cartItem = JSON.parse(localStorage.getItem("cartItem"))
+console.log(cartItem)
+
 //----------Cart  Item  Show--------------------------------------
 function showdata(){
   let products = document.getElementById("products-list");
@@ -67,18 +69,26 @@ showdata()
 function TotalItem(){
   let item = document.getElementById("total");
   item.innerText = cartItem.length;
+  // showdata()
 }
 TotalItem()
-
 //--------------Sub-Total--------------------
 function SubTotal(){
-  
+  let sub = document.getElementById("subtotal");
+  let sum = 0
+cartItem.map((el) => {
+  console.log(el.dolar)
+  sum+=el.dolar
+  console.log(sum)
+})
+sub.innerText = `$${sum}`
 }
-
+SubTotal()
 //-----------Remove---------------------------
 let remove = (id) => {
   cartItem.splice(id, 1);
   localStorage.setItem("cartItem", JSON.stringify(cartItem));
+  location.reload()
   showdata()
 }
 
